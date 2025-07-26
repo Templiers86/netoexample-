212760c0622722060fe65b3787a817a1bb37e901
Anton len <farvat24@rambler.ru>
2025-07-26 14:26:02 +0500

    Add files via upload

--- CustomsService.java
+++ CustomsService.java
@@ -1,8 +1,16 @@
 package my.netology.service;
 
 public class CustomsService {
+    public static final int Buty_rate = 100; // ставка пошлины за кг. (статическое поле, пихаем ниже с заменой)
 
     public static int calculateCustoms(int price, int weight){
-        return price / 100 + weight * 100;
+
+        int result = price/100 + weight * Buty_rate; // цена % ставку за коп. за рубль + вес * ставку пошлины в статике
+
+        return result; // "результивтивный результ" с возвратом в main
     }
+
+    // При решении этой задачки вспотел без спортзала. Тут вроде ничего сложного, но как это всё было не просто.
+    // Прям заставило шестерни со скрипов проворачиваться. Спасибо )
 }
+
